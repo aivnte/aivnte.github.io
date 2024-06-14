0ad662425a9d6a1d0515dc27657be035928f65e7
aivnte <adamjgg@icloud.com>
2024-06-14 15:33:11 +0200

    boxes done

--- js/blur-out.js
+++ js/blur-out.js
@@ -1,19 +1,27 @@
 document.addEventListener("scroll", function() {
     var subheading = document.querySelector(".subheading");
-    var rect = subheading.getBoundingClientRect();
-    var elementHeight = subheading.offsetHeight;
-    var blurThreshold = elementHeight / 100;
+    var twoBoxContainer = document.querySelector(".two-box-container");
+    
+    var elements = [subheading, twoBoxContainer];
+    
+    elements.forEach(function(element) {
+        if (element) {
+            var rect = element.getBoundingClientRect();
+            var elementHeight = element.offsetHeight;
+            var blurThreshold = elementHeight / 100;
 
-    var isInViewport = (
-        rect.top >= -blurThreshold &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + blurThreshold &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+            var isInViewport = (
+                rect.top >= -blurThreshold &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + blurThreshold &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
 
-    if (!isInViewport) {
-        subheading.classList.add("blur");
-    } else {
-        subheading.classList.remove("blur");
-    }
+            if (!isInViewport) {
+                element.classList.add("blur");
+            } else {
+                element.classList.remove("blur");
+            }
+        }
+    });
 });
