cfe6f0a54da16a21067cd8e2dfa3dd3a3a28a201
aivnte <adamjgg@icloud.com>
2024-06-18 16:06:46 +0200

    clean

--- js/blur-out.js
+++ js/blur-out.js
@@ -1,27 +1,29 @@
-document.addEventListener("scroll", function() {
-    var subheading = document.querySelector(".subheading");
-    var twoBoxContainer = document.querySelector(".two-box-container");
-    
-    var elements = [subheading, twoBoxContainer];
-    
-    elements.forEach(function(element) {
-        if (element) {
-            var rect = element.getBoundingClientRect();
-            var elementHeight = element.offsetHeight;
-            var blurThreshold = elementHeight / 100;
+document.addEventListener("scroll", function () {
+  var subheading = document.querySelector(".subheading");
+  var twoBoxContainer = document.querySelector(".two-box-container");
 
-            var isInViewport = (
-                rect.top >= -blurThreshold &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + blurThreshold &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
+  var elements = [subheading, twoBoxContainer];
 
-            if (!isInViewport) {
-                element.classList.add("blur");
-            } else {
-                element.classList.remove("blur");
-            }
-        }
-    });
+  elements.forEach(function (element) {
+    if (element) {
+      var rect = element.getBoundingClientRect();
+      var elementHeight = element.offsetHeight;
+      var blurThreshold = elementHeight / 100;
+
+      var isInViewport =
+        rect.top >= -blurThreshold &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) +
+            blurThreshold &&
+        rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth);
+
+      if (!isInViewport) {
+        element.classList.add("blur");
+      } else {
+        element.classList.remove("blur");
+      }
+    }
+  });
 });
