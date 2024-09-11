c25cbe35d782dbf629e93684a2d1faa1253f5389
Adam Griffiths <adamjgg@icloud.com>
2024-09-11 15:04:55 +0200

    clear

--- axr1/src/components/ui/button.tsx
+++
@@ -1,7 +0,0 @@
-export function Button({ children, ...props }: any) {
-  return (
-    <button {...props} className="p-2 rounded-md border">
-      {children}
-    </button>
-  );
-}
--- axr1/src/title.tsx
+++
@@ -1,7 +0,0 @@
-export function Title() {  // Capitalize the function name
-  return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      arx1
-    </h1>
-  );
-}
