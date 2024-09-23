5cec413475c3573c7459d87562e28e918e2a5010
Adam Griffiths <adamjgg@icloud.com>
2024-09-23 18:03:10 +0200

    clean

--- scripts/animation.js
+++
@@ -1,9 +0,0 @@
-const logoElement = document.getElementById('logo');
-
-// Load ASCII art from the txt file
-fetch('logo.txt')
-    .then(response => response.text())
-    .then(data => {
-        logoElement.textContent = data;
-    })
-    .catch(error => console.error('Error loading logo:', error));
